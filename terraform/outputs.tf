1d7809fd88074d226a6b6c2a0e53790fd96843d7
DEVOPS-project02 <sunilkumarreddy6321@gmail.com>
2025-05-20 22:33:37 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,7 @@ output "ec2_instance_public_ip" {
 
 output "s3_bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.artifact_bucket.bucket
+  value       = ml-app-static-uploads.bucket
 }
 
 output "kubernetes_namespace" {
